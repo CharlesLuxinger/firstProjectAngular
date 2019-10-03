c481d39748cab5228294c095a8716e8b11e88d77
Charles Luxinger <charles.luxinger@spassu.com>
2019-10-03 14:56:59 -0300

    1. Initial Commit - Start a new project and a first component

---
+++ src/app/hello/hello.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core'
+
+@Component({
+    selector: 'app-hello',
+    template: `<h2> Hello {{nome}} </h2>`
+})
+
+export class HelloComponent {
+    nome: string = 'Charles';
+}
